b8b2a1d8a11abb2a1a97dd3f9ead913d8f13f870
Dak Cheung Cheng <dc2326@princeton.edu>
2025-04-23 23:31:16 -0400

    more apps

--- app/hometown/settings.py
+++ app/hometown/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'hometown',
+    'main_app',
+    'news',
+    'payments',
 ]
 
 MIDDLEWARE = [
